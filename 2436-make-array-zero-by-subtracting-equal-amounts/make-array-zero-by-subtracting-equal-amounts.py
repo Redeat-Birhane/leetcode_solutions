0251c1abaf3410e1ad61b366398087c709a7a06f
Redeat-Birhane <149287082+Redeat-Birhane@users.noreply.github.com>
2025-08-19 22:59:24 -1100

    Time: 0 ms (100.00%) | Memory: 17.7 MB (51.78%) - LeetSync

--- 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.py
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.py
@@ -1,21 +1,5 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        nums = [num for num in nums if num != 0]
-        if not nums:
-            return 0
-        minn, maxx = min(nums), max(nums)
-        ops = 0
-        while maxx > 0:
-            for i in range(len(nums)):
-                nums[i] -= minn
-            ops += 1
-            nums = [num for num in nums if num != 0]  
-            if not nums:
-                break
-            minn, maxx = min(nums), max(nums)
-
-            
-
-        return ops
+        return len(set(num for num in nums if num > 0))
 
         
