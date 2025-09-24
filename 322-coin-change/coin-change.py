5e97fc4fbda48624129433604bad5c7385c2370d
Redeat-Birhane <149287082+Redeat-Birhane@users.noreply.github.com>
2025-09-24 01:46:26 -1100

    Time: 2338 ms (5.02%) | Memory: 443.8 MB (5.00%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,19 +1,21 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         memo, n = {}, len(coins)
-        def min_coins(i, rem_amount):
+        def few_coins(i, rem_amount):
             if i >= n:
-                return float("inf") 
+                return float("inf")
             if rem_amount == 0:
                 return 0
             if (i, rem_amount) not in memo:
                 include = float("inf")
                 if rem_amount >= coins[i]:
-                    include = 1 + min_coins(i , rem_amount - coins[i])
-                exclude = min_coins(i + 1, rem_amount)
-                memo[(i, rem_amount)] = min(include, exclude)
+                    include = 1 + few_coins(i, rem_amount - coins[i])
+                exclude = few_coins(i + 1, rem_amount)
+                memo[(i, rem_amount)] = min(exclude, include)
             return memo[(i, rem_amount)]
-        ans = min_coins(0, amount)
-        return ans if ans != float("inf")  else -1
+
+        ans = few_coins(0, amount)
+        return ans if ans != float("inf") else -1
+        
 
         
