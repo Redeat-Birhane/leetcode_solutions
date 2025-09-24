70e750c39c3ac94df090f6479ba944652e193c2e
Redeat-Birhane <149287082+Redeat-Birhane@users.noreply.github.com>
2025-09-23 19:59:30 -1100

    Time: 720 ms (5.13%) | Memory: 201.8 MB (14.20%) - LeetSync

---
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        n = len(prices)
+        memo = {}
+        def max_p(i, last_transaction):
+            if i == n:
+                return 0
+            if (i, last_transaction) not in memo:
+                if last_transaction == 0:
+                    include = max_p(i + 1, 1) - prices[i] 
+                else:
+                    include = max_p(i + 1, 0) + prices[i] - fee
+                exclude = max_p(i + 1, last_transaction)
+                memo[(i,last_transaction)] = max(include, exclude)
+            return memo[(i,last_transaction)]
+
+        return max_p(0, 0)
+                
+               
+
+
+
+
+        
