629b27c2bab80df6c2ee49ebdff58e44b7c4230e
Redeat-Birhane <149287082+Redeat-Birhane@users.noreply.github.com>
2025-09-29 10:59:29 +0300

    Time: 0 ms (100.00%) | Memory: 17.6 MB (98.35%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,12 +1,9 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        memo = {}
-        def unique_path(i, j):
-            if i == m - 1 or j == n - 1:
-                return 1
-            if (i, j) not in memo:
-                memo[(i, j)] = unique_path(i + 1, j) + unique_path(i, j + 1)
-            return memo[(i, j)]
+        dp = [[1] * n for _ in range(m)]
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
 
-        return unique_path(0, 0)
-            
+        return dp[m - 1][n - 1]
+        
