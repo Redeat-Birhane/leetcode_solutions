56562e609d8ed8fa289d40cd6d3d2635b4e04701
Redeat-Birhane <149287082+Redeat-Birhane@users.noreply.github.com>
2025-10-01 22:09:43 +0300

    Time: 490 ms (14.04%) | Memory: 127.7 MB (16.84%) - LeetSync

--- 909-stone-game/stone-game.py
+++ 909-stone-game/stone-game.py
@@ -1,4 +1,18 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        return True
+        memo = {}
+        def winner(idx, n):
+            if idx == n:
+                return piles[idx]
+            if (idx, n) not in memo:
+                memo[(idx, n)] = max(
+                piles[idx] - winner(idx + 1, n), 
+                piles[n] - winner(idx, n - 1)
+                )     
+            return memo[(idx, n)]
+
+
+        return winner(0, len(piles) - 1) > 0
+
+        
             
