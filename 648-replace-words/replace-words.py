f28f18269f4b9e83fd524ee1054daa9f7406a80c
Redeat-Birhane <149287082+Redeat-Birhane@users.noreply.github.com>
2025-10-14 22:32:05 +0300

    Time: 107 ms (51.82%) | Memory: 40.7 MB (5.04%) - LeetSync

--- 648-replace-words/replace-words.py
+++ 648-replace-words/replace-words.py
@@ -1,21 +1,37 @@
+class TrieNode:
+    def __init__(self):
+        self.children = [None for _ in range(26)]
+        self.is_end = False
+
 class Solution:
+    def __init__(self):
+        self.root = TrieNode()
+
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
-        roots, res = set(dictionary), ""
-        sentence = list(sentence.split())
-        for word in sentence:
-            curr, l = "", 0
-            while curr not in roots and l < len(word):
-                curr += word[l]
-                l += 1
-            curr += " "
-            res += curr
+        def insert(word):
+            curr = self.root
+            for char in word:
+                idx = ord(char) - ord('a')
+                if curr.children[idx] is None:
+                    curr.children[idx] = TrieNode()
+                curr = curr.children[idx]
+            curr.is_end = True
 
-        return res[:-1]
-        
+    
+        def search(word):
+            curr, res = self.root, ""
+            for char in word:
+                idx = ord(char) - ord('a')
+                if curr.children[idx] is None:
+                    return word  
+                res += char
+                curr = curr.children[idx]
+                if curr.is_end:
+                    return res
+            return word  
 
-        
+        for word in dictionary:
+            insert(word)
 
-
-
-
-        
+        replaced_words = [search(word) for word in sentence.split()]
+        return " ".join(replaced_words)
