e6153ae4bbb1f04c76640822f3548f61bb85a260
Redeat-Birhane <149287082+Redeat-Birhane@users.noreply.github.com>
2025-11-11 17:30:22 +0300

    Time: 41 ms (94.90%) | Memory: 23.3 MB (84.66%) - LeetSync

---
+++ 912-random-pick-with-weight/random-pick-with-weight.py
@@ -0,0 +1,14 @@
+import random
+class Solution:
+
+    def __init__(self, w: List[int]):
+        self.prefix = []
+        total = 0
+        for weight in w:
+            total += weight
+            self.prefix.append(total)
+        self.total = total
+
+    def pickIndex(self) -> int:
+        target = random.randint(1, self.total)
+        return bisect.bisect_left(self.prefix, target)
